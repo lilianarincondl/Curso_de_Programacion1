d401803ccd6a947b2c9ab9e056430fc70712f252
lilianarincondl <lilianarincondl@gmail.com>
2025-07-07 15:25:24 -0400

    gg

---
+++ Modulo 3/php/ejercicio1.php
@@ -0,0 +1,10 @@
+<?php
+$numero1 = 90;
+$numero2 = 4;
+
+echo "Suma: " . ($numero1 + $numero2) . "\n";         
+echo "Resta: " . ($numero1 - $numero2) . "\n";        
+echo "Multiplicación: " . ($numero1 * $numero2) . "\n"; 
+echo "División: " . ($numero1 / $numero2) . "\n";      
+echo "Módulo: " . ($numero1 % $numero2) . "\n\n";         
+?>
